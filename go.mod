089cf37cfc0e23734950356de1731bd63ccd0381
Yves Cabaneros <yves@MacBook-Pro.local>
2023-08-19 15:51:35 +0800

    setup and create simple http server

---
+++ go.mod
@@ -0,0 +1,2 @@
+module github.com/eveepaul/snippetbox
+go 1.20
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+
+import (
+  "log"
+  "net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+  if r.URL.Path != "/" {
+    http.NotFound(w, r)
+    return
+  }
+  w.Write([]byte("Hello from SnippetBox"))  
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+  w.Write([]byte("Viewing snippets"))  
+}
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+  w.Write([]byte("Creating snippet"))  
+}
+func main() {
+  mux := http.NewServeMux()
+  mux.HandleFunc("/", home)
+  mux.HandleFunc("/snippet/view", snippetView)
+  mux.HandleFunc("/snippet/create", snippetCreate)
+  
+
+  log.Println("Starting server in port:4000")
+  err := http.ListenAndServe(":4000", mux)
+  log.Fatal(err) 
+}
