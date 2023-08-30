db93c55d29443ffc8c95ffce90c07282e9f7eb12
Yves Cabaneros <yves@MacBook-Pro.local>
2023-08-30 21:15:28 +0800

    feat: implement sqlite3

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,30 +1,54 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/eveepaul/snippetbox/internal/models"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	sqliteDbFile := flag.String("sqliteDbFileName", "./database/sqlite.db", "SQLite filename")
 	flag.Parse()
 	infoLog := log.New(os.Stdout, "Info\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "Error\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*sqliteDbFile)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &models.SnippetModel{DB: db},
 	}
 	srv := &http.Server{
 		Addr: *addr, ErrorLog: errorLog, Handler: app.routes(),
 	}
 	infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(filename string) (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", filename)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
--- database/seed.sql
+++ database/seed.sql
@@ -1,16 +1,13 @@
 -- Add some dummy records (which we'll use in the next couple of chapters).
 INSERT INTO snippets (title, content, created, expires) VALUES (
 'An old silent pond',
-'An old silent pond...\nA frog jumps into the pond,\nsplash! Silence again.\n\n– Matsuo Bashō', UTC_TIMESTAMP(),
-DATE_ADD(UTC_TIMESTAMP(), INTERVAL 365 DAY)
-);
+'An old silent pond...\nA frog jumps into the pond,\nsplash! Silence again.\n\n– Matsuo Bashō', DATETIME('now'),
+DATETIME('now', '+1 year'));
 INSERT INTO snippets (title, content, created, expires) VALUES (
 'Over the wintry forest',
-'Over the wintry\nforest, winds howl in rage\nwith no leaves to blow.\n\n– Natsume Soseki', UTC_TIMESTAMP(),
-DATE_ADD(UTC_TIMESTAMP(), INTERVAL 365 DAY)
-);
+'Over the wintry\nforest, winds howl in rage\nwith no leaves to blow.\n\n– Natsume Soseki', DATETIME('now'),
+DATETIME('now', '+1 year'));
 INSERT INTO snippets (title, content, created, expires) VALUES (
 'First autumn morning',
-'First autumn morning\nthe mirror I stare into\nshows my father''s face.\n\n– Murakami Kijo', UTC_TIMESTAMP(),
-DATE_ADD(UTC_TIMESTAMP(), INTERVAL 7 DAY)
-);
+'First autumn morning\nthe mirror I stare into\nshows my father''s face.\n\n– Murakami Kijo', DATETIME('now'),
+DATETIME('now', '+7 days'));
